9e8904c9a0b1f34fd6a975ba6a4d84e363603b0e
Aleksander Olczyk <s27686@pjwstk.edu.pl>
2024-05-11 23:15:39 +0200

    Add 4 and 7x boosters

--- detection.py
+++ detection.py
@@ -4,12 +4,38 @@ import win32api
 from constants import constants
 
 
-def detect_booster():
+def detect_2x_booster():
     """Detect booster if mouse_click and earn_menu_active are True."""
     while constants.running:
         if constants.mouse_click and constants.earn_menu_active:
             try:
                 booster_pos = pyautogui.locateCenterOnScreen('assets/boosters/2x_booster.png', grayscale=True,
+                                                             confidence=0.8)
+                if booster_pos is not None:
+                    win32api.SetCursorPos(booster_pos)
+            except pyautogui.ImageNotFoundException:
+                continue
+
+
+def detect_4x_booster():
+    """Detect 4x booster if mouse_click and earn_menu_active are True."""
+    while constants.running:
+        if constants.mouse_click and constants.earn_menu_active:
+            try:
+                booster_pos = pyautogui.locateCenterOnScreen('assets/boosters/4x_booster.png', grayscale=True,
+                                                             confidence=0.8)
+                if booster_pos is not None:
+                    win32api.SetCursorPos(booster_pos)
+            except pyautogui.ImageNotFoundException:
+                continue
+
+
+def detect_7x_booster():
+    """Detect 7x booster if mouse_click and earn_menu_active are True."""
+    while constants.running:
+        if constants.mouse_click and constants.earn_menu_active:
+            try:
+                booster_pos = pyautogui.locateCenterOnScreen('assets/boosters/7x_booster.png', grayscale=True,
                                                              confidence=0.8)
                 if booster_pos is not None:
                     win32api.SetCursorPos(booster_pos)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import keyboard
 
 from clicking import click
 from constants import constants
-from detection import detect_booster, detect_money_bag
+from detection import detect_2x_booster, detect_money_bag, detect_4x_booster, detect_7x_booster
 from utils import is_earn_menu_active, on_press
 
 
@@ -14,22 +14,28 @@ def main():
     keyboard.on_press_key(constants.key_LMB, on_press)
 
     # Create threads
-    click_thread = threading.Thread(target=click, args=(constants.clicks_per_second, constants.cps_randomization))
-    booster_thread = threading.Thread(target=detect_booster)
-    money_bag_thread = threading.Thread(target=detect_money_bag)
     is_earn_menu_active_thread = threading.Thread(target=is_earn_menu_active)
+    click_thread = threading.Thread(target=click, args=(constants.clicks_per_second, constants.cps_randomization))
+    money_bag_thread = threading.Thread(target=detect_money_bag)
+    detect_2x_booster_thread = threading.Thread(target=detect_2x_booster)
+    detect_4x_booster_thread = threading.Thread(target=detect_4x_booster)
+    detect_7x_booster_thread = threading.Thread(target=detect_7x_booster)
 
     # Start threads
-    click_thread.start()
-    booster_thread.start()
-    money_bag_thread.start()
     is_earn_menu_active_thread.start()
+    click_thread.start()
+    money_bag_thread.start()
+    detect_2x_booster_thread.start()
+    detect_4x_booster_thread.start()
+    detect_7x_booster_thread.start()
 
     # Join threads
-    click_thread.join()
-    booster_thread.join()
-    money_bag_thread.join()
     is_earn_menu_active_thread.join()
+    click_thread.join()
+    money_bag_thread.join()
+    detect_2x_booster_thread.join()
+    detect_4x_booster_thread.join()
+    detect_7x_booster_thread.join()
 
 
 if __name__ == "__main__":
