b6e05a15cc1c7aca772b5354adfd29475a05fdcd
Aleksander Olczyk <s27686@pjwstk.edu.pl>
2024-05-24 14:50:37 +0200

    Update contants

--- constants.py
+++ constants.py
@@ -10,11 +10,11 @@ Class Constants:
         clicks_per_second (int): The number of mouse clicks per second.
         jitter_click_movement (bool): Determines whether mouse movements should be random during clicking.
         activation_mode (bool): Determines the activation mode. True for "hold" mode, False for "toggle" mode.
+        detect_boosters (bool): Determines whether the tool should detect boosters.
         cps_randomize (bool): Determines whether the number of clicks per second should be randomized.
 
         cps_randomization (int): The range of randomness for the number of clicks per second.
         earn_menu_active (bool): Determines whether the earn menu is active.
-        detect_boosters (bool): Determines whether the tool should detect boosters.
         mouse_moving (bool): Determines whether the mouse is currently moving.
         mouse_click (bool): Determines whether the tool should simulate mouse clicks.
         move_speed (int): The speed of mouse movement.
